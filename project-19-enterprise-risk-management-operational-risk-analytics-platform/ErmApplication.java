3b67a28a8ca4c74695593aaa6d10558ef1b0d2e1
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 16:15:51 +0300

    Create ErmApplication.java

---
+++ project-19-enterprise-risk-management-operational-risk-analytics-platform/ErmApplication.java
@@ -0,0 +1,15 @@
+package com.alhanoof.erm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ErmApplication.class,
+                args
+        );
+    }
+}
