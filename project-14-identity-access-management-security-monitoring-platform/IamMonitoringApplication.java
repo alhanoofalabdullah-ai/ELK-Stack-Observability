0b7bcd7d8eb8c9f40dadabb7bedcb5f124e5dc95
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 15:41:40 +0300

    Create IamMonitoringApplication.java

---
+++ project-14-identity-access-management-security-monitoring-platform/IamMonitoringApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamMonitoringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                IamMonitoringApplication.class,
+                args
+        );
+    }
+}
