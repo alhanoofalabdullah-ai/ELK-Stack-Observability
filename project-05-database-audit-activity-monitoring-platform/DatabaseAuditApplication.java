26c2b0a5650e6cc178379c757fa8baf565747f4f
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 13:24:27 +0300

    Create DatabaseAuditApplication.java

---
+++ project-05-database-audit-activity-monitoring-platform/DatabaseAuditApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.databaseaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAuditApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                DatabaseAuditApplication.class,
+                args
+        );
+    }
+}
