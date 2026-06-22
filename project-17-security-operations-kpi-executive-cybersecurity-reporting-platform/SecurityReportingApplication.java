f3bf95e933bb701f30022e41652a50a6731c0714
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 15:59:18 +0300

    Create SecurityReportingApplication.java

---
+++ project-17-security-operations-kpi-executive-cybersecurity-reporting-platform/SecurityReportingApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.securityreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityReportingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SecurityReportingApplication.class,
+                args
+        );
+    }
+}
