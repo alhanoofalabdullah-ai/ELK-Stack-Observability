c72ca98c77e78a9cc7461216635a1d0b8a44ecf5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 14:56:51 +0300

    Create ComplianceApplication.java

---
+++ project-10-enterprise-compliance-monitoring-regulatory-reporting-platform/ComplianceApplication.java
@@ -0,0 +1,15 @@
+package com.alhanoof.compliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ComplianceApplication.class,
+                args
+        );
+    }
+}
