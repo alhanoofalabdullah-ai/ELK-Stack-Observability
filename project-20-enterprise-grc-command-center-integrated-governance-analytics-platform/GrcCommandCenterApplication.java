3bd879555807d47de64894f040d62c8412c1c70b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 16:22:31 +0300

    Create GrcCommandCenterApplication.java

---
+++ project-20-enterprise-grc-command-center-integrated-governance-analytics-platform/GrcCommandCenterApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.grc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrcCommandCenterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                GrcCommandCenterApplication.class,
+                args
+        );
+    }
+}
