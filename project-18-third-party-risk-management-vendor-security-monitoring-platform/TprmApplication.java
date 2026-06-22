880e296882a0bb69b3f092515297969b89cb5865
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 16:07:01 +0300

    Create TprmApplication.java

---
+++ project-18-third-party-risk-management-vendor-security-monitoring-platform/TprmApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.tprm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TprmApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                TprmApplication.class,
+                args
+        );
+    }
+}
