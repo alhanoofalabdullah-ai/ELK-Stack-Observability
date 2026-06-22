ee803ae44c9271fc33097261b4552e3854a4460d
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 13:29:11 +0300

    Create UbaApplication.java

---
+++ project-06-user-behavior-analytics-insider-threat-detection-platform/UbaApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.uba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                UbaApplication.class,
+                args
+        );
+    }
+}
