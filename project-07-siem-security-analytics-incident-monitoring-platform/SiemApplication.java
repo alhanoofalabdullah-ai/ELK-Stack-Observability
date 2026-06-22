e9a0e0f167747e8fec752a03e10744a8cf6e1fec
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 14:29:36 +0300

    Create SiemApplication.java

---
+++ project-07-siem-security-analytics-incident-monitoring-platform/SiemApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.siem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SiemApplication.class,
+                args
+        );
+    }
+}
