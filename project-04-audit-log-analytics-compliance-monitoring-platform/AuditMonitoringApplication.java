039219c071db56cc45b003fd267b7aacf920c9fe
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 13:17:44 +0300

    Create AuditMonitoringApplication.java

---
+++ project-04-audit-log-analytics-compliance-monitoring-platform/AuditMonitoringApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditMonitoringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                AuditMonitoringApplication.class,
+                args
+        );
+    }
+}
