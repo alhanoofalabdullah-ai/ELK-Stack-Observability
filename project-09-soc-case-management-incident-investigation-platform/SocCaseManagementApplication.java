0aabdc4dec6630d1050e7ee404f8e2ad7b86bb48
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 14:51:21 +0300

    Create SocCaseManagementApplication.java

---
+++ project-09-soc-case-management-incident-investigation-platform/SocCaseManagementApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.soc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocCaseManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SocCaseManagementApplication.class,
+                args
+        );
+    }
+}
