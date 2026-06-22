efd7eeca4fd1ebb8996927bb3db0bfa9ee6cd4e1
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 14:44:48 +0300

    Create ThreatIntelligenceApplication.java

---
+++ project-08-threat-intelligence-security-event-correlation-platform/ThreatIntelligenceApplication.java
@@ -0,0 +1,15 @@
+package com.alhanoof.threatintel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ThreatIntelligenceApplication.class,
+                args
+        );
+    }
+}
