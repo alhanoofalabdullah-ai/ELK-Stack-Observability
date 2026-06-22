b3f3554698828d647bf50af76f7dbbc8469d1850
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 15:07:25 +0300

    Create DlpMonitoringApplication.java

---
+++ project-12-data-loss-prevention-sensitive-data-protection-platform/DlpMonitoringApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.dlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlpMonitoringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                DlpMonitoringApplication.class,
+                args
+        );
+    }
+}
